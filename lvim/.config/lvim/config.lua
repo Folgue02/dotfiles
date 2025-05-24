23feddbe49392cbb4e835b4e0f1fc21f63f673db
folgue02 <davidfolru@gmail.com>
2025-05-24 16:31:34 +0200

    NVIM: Colorscheme and lsp tweaks

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -1,3 +1,17 @@
 lvim.plugins = {
-  { 'wakatime/vim-wakatime', lazy = false}
+  { 'wakatime/vim-wakatime', lazy = false },
+  { 'blazkowolf/gruber-darker.nvim' }
 }
+
+lvim.colorscheme = 'gruber-darker'
+
+-- Disable buffer suggestions
+lvim.builtin.cmp.sources = {
+  { name = 'nvim_lsp' },
+  { name = 'path' },
+  --{ name = 'luasnip' },
+  --{ name = 'buffer' }
+}
+
+-- Options
+vim.opt.relativenumber = true
